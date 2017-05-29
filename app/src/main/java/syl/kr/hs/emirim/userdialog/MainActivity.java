7b6fbd035d6aa8f18b1bde2845662ce652375da2
soyunlee882 <soyunlee882@gmail.com>
2017-05-29 20:55:09 +0900

    #EventProgramming

--- app/src/main/java/syl/kr/hs/emirim/userdialog/MainActivity.java
+++ app/src/main/java/syl/kr/hs/emirim/userdialog/MainActivity.java
@@ -1,13 +1,33 @@
 package syl.kr.hs.emirim.userdialog;
 
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button but =(Button)findViewById(R.id.but);
+        but.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        View dlvg=View.inflate(getApplicationContext(),R.layout.dialog,null);
+        AlertDialog.Builder dialog=new AlertDialog.Builder(this);
+        dialog.setTitle("Input User Information");
+        dialog.setIcon(R.mipmap.ic_launcher);
+        dialog.setView(dlvg);
+        dialog.setPositiveButton("OK",null);
+        dialog.setNegativeButton("CANCEL",null);
+        dialog.show();
+
+
     }
 }
