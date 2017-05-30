bac2068c0a089ede91c24ff2dc932589cd6e6123
soyunlee882 <soyunlee882@gmail.com>
2017-05-30 19:28:19 +0900

    #3Event Programming 2

--- app/src/main/java/syl/kr/hs/emirim/userdialog/MainActivity.java
+++ app/src/main/java/syl/kr/hs/emirim/userdialog/MainActivity.java
@@ -1,31 +1,59 @@
 package syl.kr.hs.emirim.userdialog;
 
+import android.content.DialogInterface;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements OnClickListener{
-
+    TextView textName,textEmail;
+    EditText editName,editEmail;
+    View dlvg;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Button but =(Button)findViewById(R.id.but);
         but.setOnClickListener(this);
+        textName=(TextView)findViewById(R.id.text_name);
+        textEmail=(TextView)findViewById(R.id.text_email);
     }
 
     @Override
     public void onClick(View view) {
-        View dlvg=View.inflate(getApplicationContext(),R.layout.dialog,null);
+        dlvg=View.inflate(getApplicationContext(),R.layout.dialog,null);
         AlertDialog.Builder dialog=new AlertDialog.Builder(this);
         dialog.setTitle("Input User Information");
         dialog.setIcon(R.mipmap.ic_launcher);
         dialog.setView(dlvg);
-        dialog.setPositiveButton("OK",null);
-        dialog.setNegativeButton("CANCEL",null);
+        dialog.setPositiveButton("OK",new DialogInterface.OnClickListener(){
+            @Override
+            public void onClick(DialogInterface dialogInterface,int i){
+                editName=(EditText)dlvg.findViewById(R.id.edit_name);
+                editEmail=(EditText)dlvg.findViewById(R.id.edit_email);
+                textName.setText(editName.getText());
+                textEmail.setText(editEmail.getText());
+
+            }
+        });
+        dialog.setNegativeButton("CANCEL",new DialogInterface.OnClickListener(){
+            @Override
+            public void onClick(DialogInterface dialogInterface,int i){
+                Toast toast=new Toast(MainActivity.this);
+                View toastV=View.inflate(MainActivity.this,R.layout.toast,null);
+                TextView textToast=(TextView)toastV.findViewById(R.id.text_toast);
+                textToast.setText("cencle 되었습니다.");
+                toast.setView(toastV);
+                toast.show();
+
+            }
+        });
         dialog.show();
 
 
